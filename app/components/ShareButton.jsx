3f187d7258fd995980ab9007550363eea9ce2e25
SumonsNote <sohankhance@gmail.com>
2024-06-03 16:36:16 +0600

    changed in share

--- app/components/ShareButton.jsx
+++ app/components/ShareButton.jsx
@@ -47,7 +47,7 @@ export default function ShareButtons({ recipe }) {
 
       <a
         className="whatsapp"
-        href={`https://api.whatsapp.com/send?text=${title}: ${url}`}
+        href={`https://api.whatsapp.com/send?${url}`}
         target="_blank"
       >
         <Image src={whatsapp} alt="whatsapp" width={30} height={20} />
